5c34bb795a0706418f36ca9e504fd8d0e92b3a09
juba0713 <160235657+juba0713@users.noreply.github.com>
2024-10-14 13:08:49 +0800

    Delete Capstone-1 directory
    asdsa

--- Capstone-1/src/main/java/capstone/model/dao/ManagerEvaluatedApplicantDao.java
+++
@@ -1,9 +0,0 @@
-package capstone.model.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import capstone.model.dao.entity.ManagerEvaluatedApplicantEntity;
-
-public interface ManagerEvaluatedApplicantDao extends JpaRepository<ManagerEvaluatedApplicantEntity, Integer>{
-
-}
--- Capstone-1/src/main/java/capstone/model/dao/ManagerEvaluationDetailsDao.java
+++
@@ -1,9 +0,0 @@
-package capstone.model.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import capstone.model.dao.entity.ManagerEvaluationDetailsEntity;
-
-public interface ManagerEvaluationDetailsDao extends JpaRepository<ManagerEvaluationDetailsEntity, Integer>{
-
-}
